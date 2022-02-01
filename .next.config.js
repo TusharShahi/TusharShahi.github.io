bb7c98ad69b734f2b06d6999a43b56fc82edd422
Tushar Shahi <tusharshahi@Tushars-MacBook-Pro.local>
2022-02-02 00:17:11 +0530

    done

--- .next.config.js
+++ .next.config.js
@@ -5,6 +5,8 @@ module.exports = {
     domains: ["localhost:3000", ""],
     deviceSizes: [280, 600, 640, 750, 828, 1080, 1200, 1920, 2048, 3840]
   },
+  assetPrefix:
+    process.env.NODE_ENV === "production" ? "/TusharShahi.github.io/" : "",
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback.fs = false;
