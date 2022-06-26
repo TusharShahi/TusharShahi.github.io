5fda5dccdf90b8614a5921c388cc6b37dd628a6d
Tushar Shahi <tusharshahi@Tushars-MacBook-Pro.local>
2022-06-26 11:59:45 +0530

    images config

--- .next.config.js
+++ .next.config.js
@@ -23,6 +23,7 @@ module.exports = withOptimizedImages({
   ],
   reactStrictMode: true,
   images: {
+    loader: "custom",
     domains: ["localhost:3000", ""],
     deviceSizes: [280, 600, 640, 750, 828, 1080, 1200, 1920, 2048, 3840]
   },
