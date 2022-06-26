84ec8272cc5bd5351ef0ea33b1f2eb971382e4e9
Tushar Shahi <tusharshahi@Tushars-MacBook-Pro.local>
2022-06-26 15:16:34 +0530

    Revert "revalidate"
    This reverts commit bf14cc02cd1a3ab88055b26498ea96cd37c41a0a.

--- .next.config.js
+++ .next.config.js
@@ -6,7 +6,7 @@ const withOptimizedImages = require("next-optimized-images");
 module.exports = withOptimizedImages({
   headers: async () => [
     {
-      source: "/:all*(svg|jpg|png|webp)",
+      source: "/:all*(svg|jpg|png)",
       locale: false,
       headers: [
         {
